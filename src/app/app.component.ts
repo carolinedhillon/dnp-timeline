91b36e209ffb7e70e5d55964e0a60893b6ca91c9
Caroline Dhillon <carolinedhillon@Carolines-iMac.local>
2020-11-30 10:56:14 +0000

    Timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   timelineData = [
-    { icon: '\uf19d', category:'education' ,text: 'Lyon School', start:'01-01-1986', end:'01-01-1998'},
-    { icon: '\uf19d', category:'education', text: 'Hospitality school', start:'01-01-1998', end:'01-01-2002'},
-    { icon: '\uf19d', category:'education',  text: 'Masters in HR', start:'01-01-2016', end:'01-01-2018'},
-    { icon: '\uf19c', category:'work',  text: 'HR Hotels 1', start:'01-01-2002', end:'01-01-2008'},
-    { icon: '\uf19c', category:'work',  text: 'HR & Data IT 2', start:'01-01-2008', end:'01-01-2020'},
-    { icon: '\uf19c', category:'work',  text: 'Data Insurance 3', start:'01-01-2020', end:'01-07-2020'}
+    { icon: '\uf19d', category:'school' ,text: 'Externat Ste Marie, France', start:'01-09-1998', end:'30-06-2001'},
+    { icon: '\uf19d', category:'college', text: 'Institut Vatel, France', start:'01-09-2001', end:'30-06-2004'},
+    { icon: '\uf19d', category:'college',  text: 'University of Salford, Manchester', start:'01-05-2015', end:'01-09-2016'},
+    { icon: '\uf19d', category:'college',  text: 'The Manchester College', start:'01-10-2011', end:'01-01-2014'},
+    
+    { icon: '\uf19c', category:'work',  text: 'The Connaught', start:'01-09-2007', end:'01-12-2008'},
+    { icon: '', category:'work',  text: '', start:'01-01-2010', end:'01-01-2011'},
+    { icon: '\uf19c', category:'work',  text: 'The Langham', start:'01-12-2008', end:'01-01-2010'},
+
+    { icon: '\uf19d', category:'certification',  text: 'Prince2', start:'01-01-2020', end:'01-03-2020'},
+    { icon: '\uf13b', category:'certification',  text: 'HTML,CSS,Javascript', start:'01-02-2019', end:'01-01-2021'},
+
+    { icon: '\uf1c0', category:'work-it',  text: 'NetNames', start:'01-02-2011', end:'01-11-2015'},
+    { icon: '\uf0c2', category:'work-it',  text: 'vmware', start:'01-11-2015', end:'01-02-2020'},
+    { icon: '\uf1de', category:'work-it',  text: 'QBE', start:'01-03-2020', end:'01-07-2020'},
+    
   ]; 
 }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,255 +1,332 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, ɵSWITCH_COMPILE_INJECTABLE__POST_R3__ } from '@angular/core';
-import { selection, select, event as d3Event, selectorAll } from "d3-selection";
-import {axisBottom, axisTop} from 'd3-axis';
-import { scaleLinear, scaleTime} from 'd3-scale';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  ɵSWITCH_COMPILE_INJECTABLE__POST_R3__,
+} from '@angular/core';
+import { selection, select, event as d3Event, selectorAll } from 'd3-selection';
+import { axisBottom, axisTop } from 'd3-axis';
+import { scaleLinear, scaleTime } from 'd3-scale';
 import * as moment from 'moment';
-import {timeParse, timeFormat} from 'd3-time-format';
-import {min, max} from 'd3-array';
+import { timeParse, timeFormat } from 'd3-time-format';
+import { min, max } from 'd3-array';
 import { timeMonth, timeYear } from 'd3';
 import { dimgray } from 'color-name';
 import 'd3-selection-multi';
+import { config } from 'rxjs';
 @Component({
   selector: 'timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
-  host: { "(window:resize)": "onResize()" }
+  host: { '(window:resize)': 'onResize()' },
 })
 export class TimelineComponent implements OnInit {
-  @Input() data; 
-  @ViewChild('gantt', {static: true}) gantt: ElementRef;
+  @Input() data;
+  @ViewChild('gantt', { static: true }) gantt: ElementRef;
 
-  public config = {
-    id: "#container",
-    grid: { rows: 6, cols: 10, width: 50, height: 50 },
-    padding: { left: 100, right: 10, top: 0, bottom: 20},
-    bar: { 
-      size: { height: 20},
-      margin: { top: 5, bottom: 5}
+  public config: Config = {
+    id: '#container',
+    svg: { width: 50, height: 50 },
+    axis: { heigth: 20 },
+    category: {
+      padding: { left: 10, right: 10, top: 20, bottom: 0 },
     },
-    dateFormat: undefined,
-    dateFunc: undefined,
-    offset: { start: 2, end: 3}
+    bar: {
+      size: { height: 30 },
+      margin: { top: 5, bottom: 5 },
+      text: { top: 5 },
+    },
+    date: {
+      format: { in: '%d-%m-%Y', out: 'DD-MM-YYYY' },
+    },
+    timeline: { offset: { start: 2, end: 5 } },
   };
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
+
   ngAfterViewInit(): void {
     this.drawTimeline(this.config);
   }
+  
   onResize() {
     console.log('resize detected - redrawing');
     this.drawTimeline(this.config);
   }
-  drawTimeline(_config: any) {
-    const container = select(_config.id);
-    let { offsetHeight: height, offsetWidth: width } = container[
-      "_groups"
-    ][0][0];
 
-    _config.grid.height = Math.floor(height / _config.grid.rows);
-    _config.grid.width = Math.floor(width / _config.grid.cols);
-
-    height = Math.floor(height / _config.grid.height) * _config.grid.height;
-    width = Math.floor(width / _config.grid.width) * _config.grid.width;
-
-    this.config.dateFormat =  { in: '%d-%m-%Y', out: 'DD-MM-YYYY', func: {in:{}, out:{}}};
-    this.config.dateFunc = { in: timeParse(this.config.dateFormat.in), out: timeParse(this.config.dateFormat.out) };
-    
-
-    const svg = container
-      .html("")
-      .append("svg")
-      .attr("viewBox", `0 0  ${width} ${height}`)
-      .attr("height", height)
-      .attr("width", width);
-
-      svg.append("g")
-      .attr("class", "rows")
-      .selectAll("g")
-      .data(this.data)
-        .enter()
-        .append('g')
-        .attr('type',d=>d['icon']);
-    let svgSize = {height,width};
-    let svgScale = this.drawAxes(svg,this.data,svgSize, this.config);
-    let objList = this.assignHeight(this.data,svgSize, this.config)
-    this.drawBoxes(svg,objList,svgSize, svgScale, this.config);
-
-
-
+  processConfig(config: Config) {
+    config.date.transform = {
+      in: timeParse(config.date.format.in),
+      out: timeParse(config.date.format.out),
+    };
+    config.category.calc = {
+      heigth: config.category.padding.top + config.category.padding.bottom,
+    };
+    config.bar.calc = {
+      heigth:
+        config.bar.margin.top +
+        config.bar.margin.bottom +
+        config.bar.size.height,
+    };
+    return config;
   }
 
-  drawBoxes(svg: any, objList: any[],svgSize: SVGSize, svgScale: SVGScale, config){
-    const box = { height: (svgSize.height - (config.padding.top + config.padding.bottom)) / objList.length};
+  drawTimeline(_config: Config) {
+    const container = select(_config.id);
+    let { offsetHeight: height, offsetWidth: width } = container['_groups'][0][0];
+
+    this.config = this.processConfig(this.config);
+    let objList = this.assignHeight(this.data, this.config);
+    let uniqueList: number[] = Array.from(new Set(objList.map(obj=>obj.height)));
+    // height = uniqueList.reduce((acc, obj) => (acc += obj), 0);
+    
+    height = Math.max(...uniqueList) + _config.axis.heigth + 50;
+    objList.forEach(obj=>(obj.height = height - (obj.height + this.config.axis.heigth )));
+    // console.log({ objList, uniqueList, height});
+
+    const svgSize = { height, width };
+    const svg = this.setupSVG(container, svgSize);
+
+    let svgScale = this.drawAxes(svg, this.data, svgSize, this.config);
+
+    this.drawBoxes(svg, objList, svgSize, svgScale, this.config);
+  }
+
+  setupSVG(container, svgSize: SVGSize) {
+    return container
+      .html('')
+      .append('svg')
+      .attr('viewBox', `0 0  ${svgSize.width} ${svgSize.height}`)
+      .attr('height', svgSize.height)
+      .attr('width', svgSize.width);
+  }
+
+  drawBoxes(
+    svg: any,
+    objList: any[],
+    svgSize: SVGSize,
+    svgScale: SVGScale,
+    config: Config
+  ) {
+    // const box = { height: (svgSize.height - (config.padding.top + config.padding.bottom)) / objList.length};
     // const categories = Array.from(new Set(objList.map(obj=>obj.category)));
-    const swimlines = svg.append("g")
-      .attr("class", "swinlanes")
-      .selectAll("rect")
+    const swimlines = svg
+      .append('g')
+      .attr('class', 'swinlanes')
+      .selectAll('rect')
       .data(objList)
       .enter();
 
-    let swimlane = swimlines
-    .append('g')
-    .attr('id',d=>d.icon);
+    let swimlane = swimlines.append('g').attr('id', (d) => d.category);
 
+    const bar = swimlane.append('g').attr('id', (d) => d.text);
 
-    swimlane.append('rect')
-      .attrs((d,i)=>({ 
-        class: 'swinlane',
-        icon: d.icon,
-        x: 0, 
-        y: i* box.height + config.padding.top, 
-        width: svgSize.width, 
-        height: box.height 
-      }))
+    bar.append('rect').attrs((d, i) => ({
+      class: 'swinlane-bar',
+      x: svgScale.xScale(config.date.transform.in(d.start)),
+      // y: i* box.height + config.padding.top + 40,
+      y: d.height  - config.bar.margin.top,
+      rx:2,
+      ry:2,
+      width:
+        svgScale.xScale(config.date.transform.in(d.end)) -
+        svgScale.xScale(config.date.transform.in(d.start)),
+      height: config.bar.size.height,
+    }));
 
-    swimlane.append('text')
-      .attrs((d,i)=>({ 
-        class: 'swinlane-text',
-        x: svgSize.width, 
-        y: (i+1)* box.height + config.padding.top - 5, 
-      }))
-      .text(d=>`${d.text}`);
-
-      // swimlane.append('text')
-      // .attrs((d,i)=>({ 
-      //   class: 'swinlane-icon fa',
-      //   x: 0, 
-      //   y: (i)* box.height + config.padding.top + box.height/2, 
-      //   "font-family": 'FontAwesome',
-      //   "font-size": '2rem'}))
-      // .text(d=>`${d.icon}` ); 
-    
-    const bar = swimlane
-      .append('g')
-      .attr('id',d=>d.text);
-
-    bar.append('rect')
-      .attrs((d,i)=>({ 
-        class: 'swinlane-bar',
-        x: svgScale.xScale(config.dateFunc.in(d.start)), 
-        // y: i* box.height + config.padding.top + 40, 
-        y: d.height,
-        width: svgScale.xScale(config.dateFunc.in(d.end)) - svgScale.xScale(config.dateFunc.in(d.start)), 
-        height: 20 
-      }));
-
-    bar.append('text')
-      .attrs((d,i)=>({ 
+    bar
+      .append('text')
+      .attrs((d, i) => ({
         class: 'bar-icon fa',
-        x: svgScale.xScale(config.dateFunc.in(d.start)) - 20, 
-        // y: i* box.height + config.padding.top + 55, 
-        y: d.height + 15,
-        "font-family": 'FontAwesome',
-        "font-size": '2rem'}))
-      .text(d=>`${d.icon}` );
-
-    bar.append('text')
-      .attrs((d,i)=>({ 
-        class: 'bar-text',
-        x: svgScale.xScale(config.dateFunc.in(d.start)) , 
-        // y: i* box.height + config.padding.top + 53, 
-        y: d.height + (config.bar.size.height/2 + config.bar.margin.bottom),
+        x: svgScale.xScale(config.date.transform.in(d.start)) +10,
+        // y: i* box.height + config.padding.top + 55,
+        y: d.height + config.bar.size.height/2 ,
+        // y: d.height - config.bar.text.top,
+        'font-family': 'FontAwesome',
+        'font-size': '2rem',
       }))
-      .text(d=>d.text);
-    
+      .text((d) => `${d.icon}`);
 
+    bar
+      .append('text')
+      .attrs((d, i) => ({
+        class: 'bar-text',
+        x: svgScale.xScale(config.date.transform.in(d.start)) + 30,
+        y: d.height + config.bar.size.height / 2,
+      }))
+      .text((d) => d.text);
+
+
+      // swimlane.append('rect')
+    //   .attrs((d,i)=>({
+    //     class: 'swinlane',
+    //     icon: d.icon,
+    //     x: 0,
+    //     y: i* box.height + config.padding.top,
+    //     width: svgSize.width,
+    //     height: box.height
+    //   }))
+
+    // swimlane.append('text')
+    //   .attrs((d,i)=>({
+    //     class: 'swinlane-text',
+    //     x: svgSize.width,
+    //     y: (i+1)* box.height + config.padding.top - 5,
+    //   }))
+    //   .text(d=>`${d.text}`);
+
+    // swimlane.append('text')
+    // .attrs((d,i)=>({
+    //   class: 'swinlane-icon fa',
+    //   x: 0,
+    //   y: (i)* box.height + config.padding.top + box.height/2,
+    //   "font-family": 'FontAwesome',
+    //   "font-size": '2rem'}))
+    // .text(d=>`${d.icon}` );  
 
     swimlane.append('line')
-      .attrs((d,i)=>({ 
+      .attrs((d,i)=>({
         class: 'swinlane-separator',
-        x1: 0, 
-        x2: svgSize.width, 
-        y1: (i+1)* box.height + config.padding.top, 
-        y2: (i+1)* box.height + config.padding.top, 
-        // y1: i === objList.length -1 ? 0 : (i+1)* box.height + config.padding.top, 
-        // y2: i === objList.length -1 ? 0 : (i+1)* box.height + config.padding.top, 
+        x1: 0,
+        x2: svgSize.width,
+        y1: d.height - (config.bar.margin.bottom + config.bar.margin.top),
+        y2: d.height - (config.bar.margin.top + config.bar.margin.top),
+        // y1: i === objList.length -1 ? 0 : (i+1)* box.height + config.padding.top,
+        // y2: i === objList.length -1 ? 0 : (i+1)* box.height + config.padding.top,
       }));
-
-
-      
   }
 
-  assignHeight(objList: any[], svgSize: SVGSize, config){
-    let objMap = objList.reduce((acc,item)=>{
+  assignHeight(objList: any[], config: Config) {
+    let objMap = objList.reduce((acc, item) => {
       acc[item.category] = acc[item.category] || [];
       acc[item.category].push(item);
       return acc;
-    },{});
-    
-    let curHeight = config.padding.bottom; 
+    }, {});
+
+    let curHeight = 0;
     let prevObj;
-    Object.keys(objMap)
-      .forEach(key=>{
-        objMap[key]
-          .sort((a,b)=> moment(a.start,config.dateFormat.out).diff(moment(b.start,config.dateFormat.out)))
-          .forEach((obj,i)=>{
-          
-          if( prevObj && this.isOverLap(prevObj,obj,config)) 
-          curHeight =  curHeight + config.bar.size.height 
-            + config.bar.margin.top
-            + config.bar.margin.bottom;
-          
-          obj['height'] =  svgSize.height - curHeight - (config.padding.top + config.padding.bottom);
+    Object.keys(objMap).forEach((key) => {
+      objMap[key]
+        // .sort((a, b) =>
+        //   moment(a.start, config.date.format.out).diff(
+        //     moment(b.start, config.date.format.out)
+        //   )
+        // )
+        .forEach((obj, i) => {
+          if (!prevObj) curHeight = curHeight + config.bar.size.height + config.bar.margin.bottom + config.bar.margin.top;
+          else if (this.isOverLap(prevObj, obj, config))
+          curHeight = curHeight + config.bar.size.height + config.bar.margin.bottom + config.bar.margin.top;
+            // curHeight = curHeight +  config.bar.calc.heigth;
+          else curHeight = curHeight;
+
+          obj['height'] = curHeight;
           prevObj = obj;
+          // console.log(obj.text,'>>>', obj.height);
         });
-      });
-    console.log({objList,objMap});
+        prevObj = undefined;
+    });
+    // console.log({objList,objMap});
     return objList;
   }
-  isOverLap(a,b, config){
-    let tolerance = 12;
-    console.log(
-      a.text,
-      a.end,
-      b.text,
-      b.start,
-      moment(a.end, config.dateFormat.out).diff(moment(b.start, config.dateFormat.out),'month')
-    )
-    return moment(a.end, config.dateFormat.out).diff(moment(b.start, config.dateFormat.out),'month') < tolerance;
+  isOverLap(a, b, config: Config) {
+    const tolerance = 6;
+    const diff = moment(b.start, config.date.format.out).diff(moment(a.end, config.date.format.out),'month');
+    // console.log(a.category, a.text, b.category, b.text, diff < tolerance ,diff, tolerance);
+    console.log(`${a.category} :: ${a.text}(${a.end}) | ${b.text}(${b.start}) >> ${diff} ${diff < tolerance}`);
+    return diff < tolerance;
   }
-  drawAxes(svg: any, objList: any[],svgSize: SVGSize, config): SVGScale{
-    
-    
+  drawAxes(
+    svg: any,
+    objList: any[],
+    svgSize: SVGSize,
+    config: Config
+  ): SVGScale {
     let yScale = scaleLinear().rangeRound([svgSize.height, 0]);
-    
-    let axisStartDate: Date = min(objList, d => config.dateFunc.in(d.start));
-    let axisEndDate: Date = max(objList, d => config.dateFunc.in(d.end));
-    
-    axisStartDate = timeYear.offset(axisStartDate, config.offset.start);
-    axisEndDate = timeYear.offset(axisEndDate, config.offset.end);
-    
+
+    let axisStartDate: Date = min(objList, (d) =>
+      config.date.transform.in(d.start)
+    );
+    let axisEndDate: Date = max(objList, (d) =>
+      config.date.transform.in(d.end)
+    );
+
+    console.log({ axisStartDate, axisEndDate });
+    axisStartDate = timeYear.offset(
+      axisStartDate,
+      -config.timeline.offset.start
+    );
+    axisEndDate = timeYear.offset(axisEndDate, config.timeline.offset.end);
+    console.log({ axisStartDate, axisEndDate });
+
     let xScale = scaleTime()
       .domain([axisStartDate, axisEndDate])
-      .range([config.padding.left, svgSize.width- config.padding.right]);
+      .range([
+        config.category.padding.left,
+        svgSize.width - config.category.padding.right,
+      ]);
 
     let xAxis = axisBottom(xScale)
       .ticks(timeYear, 2)
-      .tickFormat((date: Date) => timeYear(date) < date ? timeFormat('%b')(date) : timeFormat('%Y')(date))
+      .tickFormat((date: Date) =>
+        timeYear(date) < date ? timeFormat('%b')(date) : timeFormat('%Y')(date)
+      )
       .tickSizeInner(5)
       .tickSizeOuter(0);
 
-    let axis = svg.append('g')
+    let axis = svg
+      .append('g')
       .attr('class', 'x-axis')
-      .attr('transform', 'translate(0,' + (svgSize.height -(config.padding.top+ config.padding.bottom)) + ')')
+      .attr(
+        'transform',
+        'translate(0,' +
+          (svgSize.height -
+            (config.category.padding.top + config.category.padding.bottom)) +
+          ')'
+      )
       .call(xAxis)
-      .selectAll("text")
-      .style("text-anchor", "middle");
+      .selectAll('text')
+      .style('text-anchor', 'middle');
 
     // setting the axis height dynamically
     // this.config.axis.height = axis._parents[0].getBBox().height;
-    
+
     // console.log('Axis height updated to', this.config.axis.height);
     return { xScale, yScale };
   }
-
-
 }
 
-interface SVGSize {height: number, width: number}
+interface SVGSize {
+  height: number;
+  width: number;
+}
 
 interface SVGScale {
   xScale: any;
   yScale: any;
+}
+
+interface Config {
+  id: string;
+  svg: { width: number; height: number };
+  axis: { heigth: number };
+  category: {
+    padding: { left: number; right: number; top: number; bottom: number };
+    calc?: { heigth: number };
+  };
+  bar: {
+    size: { height: number };
+    margin: { top: number; bottom: number };
+    text: { top: number };
+    calc?: { heigth: number };
+  };
+  date: {
+    format: { in: string; out: string };
+    transform?: { in: Function; out: Function };
+  };
+  timeline: {
+    offset: { start: number; end: number };
+  };
 }
